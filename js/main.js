57e8b441d02d3c19681eb0a9b799b39980424cc5
Artificial Ryan <emailsareoldschool@gmail.com>
2025-04-03 06:12:22 -0400

    Implement website improvements

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-import { renderHeader } from './header.html';
-import { renderFooter } from './footer.html';
-
-const headerElement = document.querySelector('header');
-const footerElement = document.querySelector('footer');
-
-if (headerElement) {
-  headerElement.outerHTML = renderHeader();
-}
-
-if (footerElement) {
-  footerElement.outerHTML = renderFooter();
-}
+// This file is intentionally left blank
